1cbb5b9ec1fef43bb8b81ebf5829328348a75752
Raphaël Agostini <raphael.agostini@ynov.com>
2020-01-29 15:04:56 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import logger from 'logger'
+
+import App from './App.vue'
+import config from 'config'
+import router from './router'
+import store from './store/index.js'
+import api from './interfaces/apiInterface'
+
+// prevents from display notice about running in dev mode
+Vue.config.productionTip = false
+
+// make api and log available everywhere
+Vue.prototype.$api = api
+Vue.prototype.$log = logger
+
+logger.info('config:', config);
+logger.info('main.js VERSION', VERSION); // eslint-disable-line no-undef
+
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <p>Hello word</p>
+    <ApiConsumption/>
+  </div>
+</template>
+
+<script>
+import ApiConsumption from '../components/ApiConsumption.vue'
+
+export default {
+    name: 'home',
+    components: {
+        ApiConsumption
+    }
+}
+</script>
